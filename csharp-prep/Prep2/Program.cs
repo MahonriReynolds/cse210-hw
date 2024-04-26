d8fe9d0dc4417a3231be05375ab8ba03870f1d4f
MahonriReynolds <mahonri.r66@gmail.com>
2024-04-26 17:04:14 -0600

    C# Prep 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Grade Percentage: ");
+        string gradePercentageString = Console.ReadLine();
+        int gradePercentageInt = int.Parse(gradePercentageString);
+
+        char letter;
+        if (gradePercentageInt >= 90)
+        {
+            letter = 'A';
+        }
+        else if (gradePercentageInt >= 80)
+        {
+            letter = 'B';
+        }
+        else if (gradePercentageInt >= 70)
+        {
+            letter = 'C';
+        }
+        else if (gradePercentageInt >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        int lastDigit = gradePercentageInt % 10;
+        char sign = '-';
+        if (lastDigit >= 7)
+        {
+            sign = '+';
+        }
+
+        if ((letter == 'A' && sign == '+') || letter == 'F')
+        {
+            sign = '\0';
+        }
+           
+        Console.WriteLine($"{letter}{sign}");
+            
+
+        if (gradePercentageInt >= 70)
+        {
+            Console.WriteLine("Passed the course");
+        }
+        else
+        {
+            Console.WriteLine("Passedn't the course");
+        }
+
     }
 }
