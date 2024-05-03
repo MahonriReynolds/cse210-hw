c7ad0280a86f29a0a3366b8186e69bdba5166121
MahonriReynolds <mahonri.r66@gmail.com>
2024-05-02 18:40:37 -0600

    prep 3 done with stretch challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        bool play_next_round = true;
+
+        while (play_next_round)
+        {
+
+            Random randomGenerator = new();
+            int magic_number = randomGenerator.Next(1, 101);
+
+            int user_guess = 0;
+            int guess_count = 0;
+
+            while (user_guess != magic_number)
+            {
+                Console.Write("What is your guess? ");
+                user_guess = int.Parse(Console.ReadLine());
+                guess_count++;
+
+                if (user_guess == magic_number)
+                {
+                    Console.Write("You guessed it!\n");
+                }
+                else if (user_guess < magic_number)
+                {
+                    Console.Write("Higher\n");
+                }
+                else
+                {
+                    Console.Write("Lower\n");
+                }
+
+            }
+
+            Console.Write($"Guessed in {guess_count} guess(s)\n");
+
+            Console.Write("Play again? (yes/no) ");
+            string play_again_answer = Console.ReadLine();
+            if (play_again_answer != "yes")
+            {
+                play_next_round = false;
+            }
+        }
     }
 }
