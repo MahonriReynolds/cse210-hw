956e7db96a354c5b19298252968b5eb42e41ea17
MahonriReynolds <mahonri.r66@gmail.com>
2024-05-02 19:21:28 -0600

    prep 4 completed with stretch challenge

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
-using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> user_numbers = new List<int>();
+
+        Console.Write("Enter a list of numbers, type 0 when finished.\n");
+
+        int next_user_num;
+        do 
+        {
+            Console.Write("Enter a number: ");
+            next_user_num = int.Parse(Console.ReadLine());
+            if (next_user_num != 0)
+            {
+                user_numbers.Add(next_user_num);
+            }
+        } while (next_user_num != 0);
+        
+        Console.Write($"The sum is: {user_numbers.Sum()}\n");
+        Console.Write($"The average is: {user_numbers.Average()}\n");
+        Console.Write($"The largest number is: {user_numbers.Max()}\n");
+
+        Console.Write($"The smallest positive number is: {user_numbers.Where(i => i > 0).Min()}\n");
+
+        Console.Write($"The sorted list is:\n");
+        user_numbers.Sort();
+        foreach(var num in user_numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
